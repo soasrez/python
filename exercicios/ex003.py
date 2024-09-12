b66288a67c47d87cf22f6d40c94af377e413fc85
soasrez <soasres@outlook.com>
2024-09-12 13:56:02 -0300

    Initial commit

---
+++ exercicios/ex003.py
@@ -0,0 +1,5 @@
+n1 = int(input('Digite um número: '))
+n2 = int(input ('Digite outro número: '))
+soma = n1 + n2
+#print('A soma entre {}' .format(n1), 'e {}' .format(n2), 'é de {}.' .format(soma))
+print('A soma entre {} e {} é de {}' .format(n1, n2, soma))
---
+++ scripts-python/desafio03.py
@@ -0,0 +1,4 @@
+numero1 = input('numero 1 =')
+numero2 = input('numero 2 =')
+soma = (numero1) + (numero2)
+print('A soma entre esses números é de ' + soma)
