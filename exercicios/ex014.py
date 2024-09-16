f0af9f60b01e34266f516b8ebdc94f2f2cd5a71d
soasrez <soasres@outlook.com>
2024-09-16 15:40:37 -0300

    ex014

---
+++ exercicios/ex014.py
@@ -0,0 +1,5 @@
+c = float(input('Informe a temperatura em °c: '))
+f = ((9*c)/5)+32
+# 9 * c / 5 + 32
+#nao é necessario os parenteses se estiver na ordem de precedencia, como nesta conta por exemplo, multiplicação primeiro depois divisao e ai a soma!
+print('A temperatura  de {}°c corresponde a {}°f'.format(c, f))
